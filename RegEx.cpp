d5850359b1b7ee27233f5a95ba5f26da72e2a8b1
Angel Sedmakov <angelsedmacov2001@abv.bg>
2020-12-30 13:16:15 +0200

    fixing bugs, now it works for the whole string

--- RegEx.cpp
+++ RegEx.cpp
@@ -9,43 +9,54 @@ void normalize(string& reg)
         if(reg[i]=='\\')swap(reg[i],reg[i+1]);
     }
 }
-bool isContaint(string str, string reGex)
+bool isContaint(string str, string reGex,int indexStr)
 {
-
-        int indexStr = str.size();
         for(int indexRegex = reGex.size();indexRegex>=0;indexRegex--)
         {
+            if(indexStr<0 && indexRegex>0)return 0;
             switch(reGex[indexRegex])
             {
                 case '.':break;
                 case '?':
                     if(str[indexStr]==reGex[--indexRegex])indexStr--;
+                    indexRegex--;
                     break;
                 case '*':
-                    while(str[indexStr]==reGex[--indexRegex])indexStr--;
+                    while(str[indexStr]==reGex[--indexRegex] && indexStr>=0)indexStr--;
+                    indexRegex--;
                     break;
                 case '+':
                     if(str[indexStr]==reGex[--indexRegex])indexStr--;
                      else return 0;
-                    while(str[indexStr]==reGex[--indexRegex])indexStr--;
+                    while(str[indexStr]==reGex[--indexRegex] && indexStr>=0)indexStr--;
+                    indexRegex--;
                     break;
                 case '\\':indexRegex--;
                 default :
                     if(str[indexStr]==reGex[indexRegex])indexStr--;
                         else return 0;
             }
+
         }
         return 1;
 
+}
+bool findRegex(string str,string reg)
+{
+    for(int i=str.size();i>=0;i--)
+    {
+        if(isContaint(str,reg,i))return 1;
+    }
+    return 0;
 }
 int main()
 
 {
 
- string str = "cbba";
- string reg = ".ba?";
+ string str = "aaaaaaaaaaa";
+ string reg = "a+";
  //normalize(reg);
- cout<<isContaint(str,reg);
+ cout<<findRegex(str,reg);
 
 return 0;
 
