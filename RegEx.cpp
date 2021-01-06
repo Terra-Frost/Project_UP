7a6b0c5fb369fd478e46fea45fd2efedf9546c4b
Angel Sedmakov <angelsedmacov2001@abv.bg>
2021-01-06 22:06:46 +0200

    adding functionality for {?,+,*} to be abale to work with escped charecters

--- RegEx.cpp
+++ RegEx.cpp
@@ -2,66 +2,71 @@
 #include  <cstdlib>
 #include  <fstream>
 using  namespace  std;
+//Finding if a regex is corec,retuning aporiet arror message if it's not, and making changes for esyer work of the program
 int normalize(string& reg)
 {
     const string errorSymbols = "\\^.*+?";
-    int numberOfSpeshalChar=0;
+    int numberOfSpeshalChar = 0;
 
-    if(reg[reg.size()-1]=='\\')return -1;
-    //cout<< reg[0]<<  " "<<errorSymbols.find(reg[0])<<" "<<std::npos<<endl;
-    if(errorSymbols.find(reg[0],3)!=string::npos)return -2;
-    for(int i=0;i<reg.size();i++)
+    if(reg[reg.size()-1]=='\\') return -1;
+
+    if(errorSymbols.find(reg[0],3) != string::npos)return -2;
+    for(int i = 0; i < reg.size() ; i++)
     {
-        if(reg[i]=='\\'){
 
-                if(errorSymbols.find(reg[i+1])==string::npos)return -3;
+        if(reg[i] == '\\'){
+
+                if(errorSymbols.find(reg[i+1]) == string::npos) return -3;
                     swap(reg[i],reg[i+1]);
                     i++;
-
         }
-        else{
+        else
+        {
             int specialSymbol = errorSymbols.find(reg[i],1);
-            if(specialSymbol!=-1)
+            if(specialSymbol != -1)
             {
-                if(specialSymbol>2 && ++numberOfSpeshalChar>=2) return -4;
-                if(errorSymbols.find(reg[i+1],3)!=string::npos)return -5;
+                if(specialSymbol > 2 && ++numberOfSpeshalChar >= 2) return -4;
+                if(errorSymbols.find(reg[i+1],3) != string::npos) return -5;
             }
         }
 
-        if(reg[i]=='^' && i!=0)return -6;
+        if(reg[i] == '^' && i != 0) return -6;
 
     }
     return 0;
 }
 bool isContaint(string str, string reGex,int indexStr)
 {
-        for(int indexRegex = reGex.size()-1;indexRegex>=0;indexRegex--)
+        for(int indexRegex = reGex.size()-1 ; indexRegex >= 0; indexRegex--)
         {
            // cout<<indexRegex<<endl;
            //cout<<str[indexStr]<<"->"<<(int)str[indexStr]<<" "<<reGex[indexRegex]<<"->"<<(int)reGex[indexRegex]<<" "<<reGex[indexRegex-1]<<"->"<<(int)reGex[indexRegex-1]<<endl;
-            if(indexStr<0)return 0;
+            if(indexStr < 0) return 0;
             switch(reGex[indexRegex])
             {
                 case '^':
-                    if(str[indexStr]=='\n')return 1;
+                    if(str[indexStr] == '\n') return 1;
                     else return 0;
-                    break;
-                case '?':
-                    if(str[indexStr]==reGex[--indexRegex])indexStr--;
                     break;
                 case '.':
                     indexStr--;
                     break;
+                case '?':
+                    if(reGex[indexRegex-1] == '\\') indexRegex--;
+                    if(str[indexStr] == reGex[--indexRegex]) indexStr--;
+                    break;
                 case '+':
-                    if(str[indexStr]==reGex[indexRegex-1])indexStr--;
+                    if(reGex[indexRegex-1] == '\\') indexRegex--;
+                    if(str[indexStr] == reGex[indexRegex-1]) indexStr--;
                      else return 0;
                 case '*':
+                    if(reGex[indexRegex] == '*' && reGex[indexRegex-1] == '\\') indexRegex--;
                     indexRegex--;
-                    while(str[indexStr]==reGex[indexRegex] && indexStr>=0)indexStr--;
+                    while(str[indexStr] == reGex[indexRegex] && indexStr >= 0)indexStr--;
                     break;
                 case '\\':indexRegex--;
                 default :
-                    if(str[indexStr]==reGex[indexRegex])indexStr--;
+                    if(str[indexStr] == reGex[indexRegex])indexStr--;
                         else return 0;
                         break;
             }
@@ -76,11 +81,11 @@ bool findRegex(string str,string reg)
    // cout<<str<<endl;
     str='\n'+str;
 
-    for(int i=str.size()-1;i>=0;i--)
+    for(int i = str.size()-1; i>=0; i--)
     {
         //cout<<i<<endl;
 
-        if(isContaint(str,reg,i))return 1;
+        if(isContaint(str,reg,i)) return 1;
        // cout<<endl;
     }
    // cout<<endl;
@@ -107,11 +112,11 @@ int main()
     getline(cin,fileName);
 
     int errorCode = normalize(regEx);
-    if(errorCode!=0){
+    if(errorCode != 0) {
         cout<<errorCode;
         return 0;
     }
-    int index =1;
+    int index = 1;
  do
     {
         switch(fReadStr(fileName,str,index))
